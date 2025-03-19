9ecb2e92a1f01fcf39a6fc8ed69107c9daa667cd
S0siskaKiller <81315528+S0siskaKiller@users.noreply.github.com>
2025-03-19 15:36:43 +0300

    Добавлена функция установки программ

--- оболочкаНСА.py
+++ оболочкаНСА.py
@@ -1,7 +1,41 @@
 import os
 import getpass
 import readline
+import subprocess
 import rlcompleter
+import configparser 
+
+CONFIG_FILE = os.path.expanduser("nsacfg.ini")
+
+def load_config():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    return config
+
+def save_config(config):
+    with open(CONFIG_FILE, "w") as configfile:
+        config.write(configfile)
+
+def PMinstall(package_name,distro):
+    if distro == "Debian":
+        subprocess.run(["sudo", "apt", "install", package_name])
+    elif distro == "Arch":
+        subprocess.run(["sudo", "pacman", "-S", package_name])
+    elif distro == "Void":
+        subprocess.run(["sudo", "xbps-install", "-y", package_name])
+    elif distro == "Gentoo":
+        subprocess.run(["sudo", "emerge", package_name])
+    elif distro == "FreeBSD":
+        subprocess.run(["sudo", "pkg", "install", package_name])
+    elif distro == "OpenBSD":
+        subprocess.run(["doas", "pkg_add", package_name])
+    elif distro == "NetBSD":
+        subprocess.run(["doas", "pkgin", "install", package_name])
+    elif distro == "LFS":
+        print("похоже вам придётся компилировать все программы вручную ;)")
+        print("но если LFS всё же не ваш дистрибутив... измените строку distro в nsacfg.ini")
+    else:
+        print("ваш дистрибутив не поддерживается.")
 
 команды = {
     "ред": "vim",
@@ -20,23 +54,31 @@ import rlcompleter
     "удл": "rm",
     "прм": "mv",
     "календарь": "cal",
-    "воиспроизвести": "mpv"
+    "воиспроизвести": "mpv",
 }
 
-command_list = list(команды.keys()) + ["выход", "сд"] 
-
+command_list = list(команды.keys()) + ["выход", "сд","установить"] 
 
 def completer(text, state):
-    """Функция для автодополнения."""
     options = [x for x in command_list if x.startswith(text)]
     if state < len(options):
         return options[state]
     else:
         return None
 
-
 readline.set_completer(completer)
 readline.parse_and_bind("tab: complete")
+
+config = load_config()
+if 'settings' not in config:
+    config['settings'] = {}
+
+distro = config['settings'].get('distro')
+
+if not distro:
+    distro = input("Какой дистрибутив вы используете?: ")
+    config['settings']['distro'] = distro
+    save_config(config)
 
 while True:
     пользователь = getpass.getuser()
@@ -45,10 +87,10 @@ while True:
     try:
         вса = input(строка)
     except EOFError:
-        print("\nExiting...")
+        print("nExiting...")
         break
     except KeyboardInterrupt:
-        print("\nInterrupting...")
+        print("nInterrupting...")
         continue
 
     parts = вса.split()
@@ -69,6 +111,11 @@ while True:
             print(f"'{argument}' не является директорией.")
         except OSError as e:
             print(f"Ошибка: {e}")
+    elif command == "установить":
+        if argument:
+             PMinstall(argument,distro) 
+        else:
+            print("Укажите имя пакета для установки.")
     elif command in команды:
         command_to_execute = команды[command]
         full_command = command_to_execute
